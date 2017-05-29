60bc887070087cd90c74ce99d760cd6176caad8e
mohammad mahmoudi <mm580486@gmail.com>
2017-05-29 07:22:27 +0000

    fix typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   has_many :pages
   has_many :categories
   has_many :products, :dependent => :destroy
-  # has_many :favorites, :dependent => :destroy
+  has_many :favorites, :dependent => :destroy
   has_many :passive_favorites, class_name:  "Favorite",
              foreign_key: "user_id",
              dependent:   :destroy
