3d1dda6a3fa0694daa9fc5780120a2bf01e86f3f
mohammad mahmoudi <mm580486@gmail.com>
2017-05-31 14:52:51 +0000

    fix registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,29 @@
 class RegistrationsController < Devise::RegistrationsController
+ 
+ 
+ 
+   def create
+    build_resource(registration_params)
+
+    if resource.save
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        sign_up(resource_name, resource)
+        respond_with resource, :location => after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+        respond_with resource, :location => after_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords
+      respond_with resource
+    end
+  end  
+
+
+  
+ 
+ 
   def update_resource(resource, params)
     if resource.encrypted_password.blank? # || params[:password].blank?
       resource.email = params[:email] if params[:email]
@@ -14,4 +39,12 @@ class RegistrationsController < Devise::RegistrationsController
       resource.update_with_password(params)
     end
   end
+  
+    private
+
+  def registration_params
+    params.require(:user).permit(:email,:name, :title_id, :first_name, :last_name, 
+      :province_id, :password, :password_confirmation)
+  end
+  
 end
