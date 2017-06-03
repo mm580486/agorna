d55141feb8b679e9a47d8a28cfbb7cc3cf97d222
root <root@NMLV003.cs1local>
2017-06-03 12:43:09 +0000

    fix action mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
 
 
 
-config.action_mailer.delivery_method = :smtp
+config.action_mailer.delivery_method = :sendmail
 config.action_mailer.smtp_settings = {
   :address => "localhost",
   :port => 25,
