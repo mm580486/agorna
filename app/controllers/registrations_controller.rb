22b4630262422f036626a9487cee7cb4c2c7c23a
mohammad mahmoudi <mm580486@gmail.com>
2017-06-03 16:00:40 +0000

    add welcome email method to registration controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,11 +4,11 @@ class RegistrationsController < Devise::RegistrationsController
  
    def create
     build_resource(registration_params)
-
     if resource.save
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_navigational_format?
         sign_up(resource_name, resource)
+        UserMailer.welcome_email(resource).deliver_now
         respond_with resource, :location => after_sign_up_path_for(resource)
       else
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
