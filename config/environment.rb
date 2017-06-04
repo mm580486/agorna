8dcbfa1500fbd5dd6f5f369df74ddfa855379dbb
mohammad mahmoudi <mm580486@gmail.com>
2017-06-04 06:46:06 +0000

    remove parrent dive and add red-text class

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,16 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
+ActionView::Base.field_error_proc = Proc.new { |html_tag, instance| 
+  msg = instance.error_message 
+
+  if html_tag =~ /<(input|textarea|select)[>]+class=/
+    class_attribute = html_tag =~ /class=['"]/ 
+    html_tag.insert(class_attribute + 7, "red-text ") 
+  elsif html_tag =~ /<(input|textarea|select)/
+    first_whitespace = html_tag =~ /\s/ 
+    html_tag[first_whitespace] = " class='red-text' "
+  end 
+
+  html_tag
+}
