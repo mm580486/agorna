b0b37b046f010759a86f32252f47b5df53c82d99
mohammad mahmoudi <mm580486@gmail.com>
2017-06-05 15:07:17 +0000

    fix

--- config/initializers/cloudinary.rb
+++
@@ -1,6 +0,0 @@
-Cloudinary.config do |config|
-  config.cloud_name = 'dtihgv1c6'
-  config.api_key = '385698855943434'
-  config.api_secret = 'J2Zd1GBlbwlszIgqsZFUvlUBkWA'
-  config.cdn_subdomain = true
-end
