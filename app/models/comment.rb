e47621b2e1f6b0f2860318870e9452bd11997004
mohammad mahmoudi <mm580486@gmail.com>
2017-06-07 09:28:15 +0000

    add model validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :page
   belongs_to :product
+  validates_length_of :body, within: 2..300, too_long: 'pick a shorter name', too_short: 'pick a longer name'
 end
