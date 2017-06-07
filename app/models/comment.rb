eea4d4ca4b350aaf105a1a08e88f7933281599eb
mohammad mahmoudi <mm580486@gmail.com>
2017-06-07 10:04:21 +0000

    fix validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,5 @@ class Comment < ActiveRecord::Base
   belongs_to :page
   belongs_to :product
   validates_length_of :body, within: 2..300, too_long: 'pick a shorter name', too_short: 'pick a longer name'
+  validates_presence_of :user_id
 end
