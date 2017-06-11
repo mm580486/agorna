538556e641e2a3fb13d68aa7272f82a0c036901e
mohammad mahmoudi <mm580486@gmail.com>
2017-06-11 11:54:24 +0000

    fix

--- app/models/form_user.rb
+++ app/models/form_user.rb
@@ -1,5 +1,7 @@
 class FormUser < User
   attr_accessor :current_password
+    attr_accessor :phone
+  attr_accessor :login
 
   validates_presence_of   :email, if: :email_required?
   validates_uniqueness_of :email, allow_blank: true, if: :email_changed?
@@ -8,6 +10,11 @@ class FormUser < User
   validates_presence_of     :password, if: :password_required?
   validates_confirmation_of :password, if: :password_required?
   validates_length_of       :password, within: Devise.password_length, allow_blank: true
+def self.find_for_database_authentication warden_conditions
+  conditions = warden_conditions.dup
+  login = conditions.delete(:login)
+  where(conditions).where(["lower(phone) = :value OR lower(email) = :value", {value: login.strip.downcase}]).first
+end
 
   def password_required?
     return false if email.blank?
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :phone
+  attr_accessor :phone
   attr_accessor :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
