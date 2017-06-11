8d625af529aa4e2c1ecaea6ada49e67af503f7c9
mohammad mahmoudi <mm580486@gmail.com>
2017-06-11 21:09:10 +0000

    unique permalink

--- app/models/product_field.rb
+++ app/models/product_field.rb
@@ -2,4 +2,5 @@ class ProductField < ActiveRecord::Base
   belongs_to :product_type,required: true
   acts_as_list scope: :product_type_id,top_of_list: 0
   has_many :props
+  validates_uniqueness_of :permalink , message: 'قبلا با این نام ثبت شده است !',scope: :product_type_id
 end
