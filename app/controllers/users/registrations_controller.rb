3da1a4969db4c6f4f522adffc88c5b2e4683f7ed
mohammad mahmoudi <mm580486@gmail.com>
2017-06-11 18:29:25 +0000

    fix

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,23 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   
     def create
-    build_resource(registration_params)
-    if resource.save
-      
-      if resource.active_for_authentication?
-        set_flash_message :notice, :signed_up if is_navigational_format?
-        sign_up(resource_name, resource)
-        
-        respond_with resource, :location => after_sign_up_path_for(resource)
-      else
-        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
-        respond_with resource, :location => after_sign_up_path_for(resource)
-      end
-      
-    else
-      clean_up_passwords
-      respond_with resource
-    end
+super
   end  
 
 
