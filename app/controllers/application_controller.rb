305d21327e3e613a4a3c2a0d4ef7df5d3154b9e2
mohammad mahmoudi <mm580486@gmail.com>
2017-06-11 09:28:42 +0000

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
-protected
-def configure_permitted_parameters
-   
-   devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :phone, :email, :password, :remember_me) }
-   
-end
+
   
   def is_login
     return !current_user.nil?
@@ -93,7 +88,13 @@ end
     
   end
   
-  
+    protected
+
+  def configure_permitted_parameters
+    added_attrs = [:phone, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
   
   
 end
