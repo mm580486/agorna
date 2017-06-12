9b6dee0c4d27bf427d3452eaa20321f9ff258cef
mohammad mahmoudi <mm580486@gmail.com>
2017-06-12 07:32:48 +0000

    fix

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -20,7 +20,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     @user = @identity.user || current_user
     if @user.nil?
-      @user = User.create( email: @identity.email || "" ,name: @identity.name)
+      @user = User.create( email: @identity.email || "" ,name: @identity.name,phone: @identity.phone)
       @identity.update_attribute( :user_id, @user.id )
     end
 
