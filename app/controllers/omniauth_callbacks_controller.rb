bf099a49f5611fbe96df8495780181b36338b6da
mohammad mahmoudi <mm580486@gmail.com>
2017-06-14 07:39:31 +0000

    fix

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -19,26 +19,29 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @identity = Identity.find_for_oauth env["omniauth.auth"]
 
     @user = @identity.user || current_user
-    if @user.nil?
-      @user = User.create( email: @identity.email ,name: @identity.name,phone: rand(9999..9999999999))
-      @identity.update_attribute( :user_id, @user.id )
-    end
+    
+    render text: @user
+    
+    # if @user.nil?
+    #   @user = User.create( email: @identity.email ,name: @identity.name,phone: rand(9999..9999999999))
+    #   @identity.update_attribute( :user_id, @user.id )
+    # end
 
-    if @user.email.blank? && @identity.email
-      @user.update_attribute( :email, @identity.email)
-    end
+    # if @user.email.blank? && @identity.email
+    #   @user.update_attribute( :email, @identity.email)
+    # end
 
-    if @user.persisted?
-      @identity.update_attribute( :user_id, @user.id )
-      # This is because we've created the user manually, and Device expects a
-      # FormUser class (with the validations)
-      @user = FormUser.find @user.id
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: provider.capitalize) if is_navigational_format?
-    else
-      session["devise.#{provider}_data"] = env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
+    # if @user.persisted?
+    #   @identity.update_attribute( :user_id, @user.id )
+    #   # This is because we've created the user manually, and Device expects a
+    #   # FormUser class (with the validations)
+    #   @user = FormUser.find @user.id
+    #   sign_in_and_redirect @user, event: :authentication
+    #   set_flash_message(:notice, :success, kind: provider.capitalize) if is_navigational_format?
+    # else
+    #   session["devise.#{provider}_data"] = env["omniauth.auth"]
+    #   redirect_to new_user_registration_url
+    # end
   end
   
   
