61622bdde0f41828301d24fca55dad5b8f3851df
mohammad mahmoudi <mm580486@gmail.com>
2017-06-19 12:59:07 +0000

    fix

--- app/controllers/api/v1_controller.rb
+++ app/controllers/api/v1_controller.rb
@@ -341,7 +341,7 @@ def favorite
     
     def hasNewTickets
         @user=User.find_by_authentication_token(params[:token])
-        @tickets=Ticket.where('user_id = ? OR user_two = ?',@user.id,@user.id).order('id DESC').tikcetmessages
+        @tickets=Ticketmessage.where(id: Ticket.where('user_id = ? OR user_two = ?',@user.id,@user.id).order('id DESC').ids)
         @tickets.where.not(user_id: @user.id).update_all(seen: true)
         render json: @tickets
         
