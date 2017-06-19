5b63a657dca2d448347e65ba644593ee8056b8f6
mohammad mahmoudi <mm580486@gmail.com>
2017-06-19 07:47:43 +0000

    fix

--- app/controllers/public/home_controller.rb
+++ app/controllers/public/home_controller.rb
@@ -123,7 +123,7 @@ class Public::HomeController < ApplicationController
     private
     
     def exposition_white_list
-        params.require(:user).permit(:identify,:password,:password_confirmation,:static_phone,:avatar,:background_image,:category_id,:email,:name,:phone,:exposition_name,:exposition_detail,:exposition_address,:instagram_id,:telegram,:post_service)
+        params.require(:user).permit(:longitude,:latitude,:identify,:password,:password_confirmation,:static_phone,:avatar,:background_image,:category_id,:email,:name,:phone,:exposition_name,:exposition_detail,:exposition_address,:instagram_id,:telegram,:post_service)
     end
     
 end
