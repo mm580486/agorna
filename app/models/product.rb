45663d48bb3a961f689ddc35fe1af7ab25ae3bd2
mohammad mahmoudi <mm580486@gmail.com>
2017-06-28 11:20:43 +0000

    fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,12 @@
-
 class Product < ActiveRecord::Base
-  
   belongs_to :user
   belongs_to :product_type
   has_many :favorites, :dependent => :destroy
   has_many :comments,:dependent => :destroy,:foreign_key=>'product_id'
+  validates_presence_of :category_id
+  validates_presence_of :detail
   mount_uploaders :images, ProductUploader
   serialize :images, JSON
   serialize :properties, Hash
-  
-  
-  
-  
-  
 end
 
