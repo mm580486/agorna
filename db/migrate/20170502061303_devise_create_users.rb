e52d7c594ecab6b1c68f733e7e5ff13f6984936c
mohammad mahmoudi <mm580486@gmail.com>
2017-07-01 09:56:31 +0000

    fix

--- db/migrate/20170502061303_devise_create_users.rb
+++ db/migrate/20170502061303_devise_create_users.rb
@@ -70,7 +70,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
+    add_index :users, :email
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     add_index :users, :authentication_token, unique: true
