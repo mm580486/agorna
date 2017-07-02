452168c27c4297c46661b68c6ecee786d5e8f1eb
root <mm580486@gmail.com>
2017-07-02 05:46:51 +0000

    run migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170611075651) do
+ActiveRecord::Schema.define(version: 20170702052701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,14 @@ ActiveRecord::Schema.define(version: 20170611075651) do
   end
 
   add_index "identities", ["user_id"], name: "index_identities_on_user_id", using: :btree
+
+  create_table "marketer_tasks", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "user_two"
+    t.string   "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pages", force: :cascade do |t|
     t.integer  "user_id"
