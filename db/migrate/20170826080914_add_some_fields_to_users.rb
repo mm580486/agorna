631466253c8b83a23118775598e4e11948afe0c3
mac <mm580486@gmail.com>
2017-08-26 13:01:03 +0430

    add gender and roolback feature

--- db/migrate/20170826080914_add_some_fields_to_users.rb
+++ db/migrate/20170826080914_add_some_fields_to_users.rb
@@ -4,5 +4,15 @@ class AddSomeFieldsToUsers < ActiveRecord::Migration
     add_column :users, :national_card_image, :string
     add_column :users, :business_license_image, :string
     add_column :users, :family, :string
+    add_column :users, :gender, :string
   end
+  
+  def down
+    remove_column :users, :date, :string
+    remove_column :users, :national_card_image, :string
+    remove_column :users, :business_license_image, :string
+    remove_column :users, :family, :string
+    remove_column :users, :gender, :string
+  end
+
 end
