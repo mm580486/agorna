150a95813449ce68c70b742a565de2196d1e079b
mac <mm580486@gmail.com>
2017-08-28 11:21:41 +0430

    fix

--- app/controllers/api/v1_controller.rb
+++ app/controllers/api/v1_controller.rb
@@ -418,6 +418,8 @@ def favorite
         if params[:message].to_s != "undefined"
         @ticketmessage=Ticket.where('(user_id = ? OR user_two = ?) OR (user_id = ? OR user_two = ? )',@user.id,params[:id],params[:id],@user.id).first.ticketmessages.build(user_id: @user.id,message: params[:message])
         @ticketmessage.save
+        else
+            return false
         end
     end
     def checkNewMessage
