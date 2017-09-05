e7a6b9a6cb3274876dc846d87252ac0871248f42
mac <mm580486@gmail.com>
2017-09-05 12:57:18 +0430

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-
+config.serve_static_assets = true
 
 config.action_mailer.delivery_method = :sendmail
 
