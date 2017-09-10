3f2c03dd1e4fe946f532495579ca861522a4bb54
mac <mm580486@gmail.com>
2017-09-10 14:01:10 +0430

    add enamad logo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170702052701) do
+ActiveRecord::Schema.define(version: 20170826080914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -254,6 +254,11 @@ ActiveRecord::Schema.define(version: 20170702052701) do
     t.string   "authentication_token",                   null: false
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
+    t.string   "date"
+    t.string   "national_card_image"
+    t.string   "business_license_image"
+    t.string   "family"
+    t.string   "gender"
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
