89b10e814e6db4b3a64d357f0a6e99b87776c86a
mac <mm580486@gmail.com>
2017-10-02 17:22:17 +0330

    fix

--- app/controllers/admin/forms_controller.rb
+++ app/controllers/admin/forms_controller.rb
@@ -86,10 +86,11 @@ class Admin::FormsController < ApplicationController
   def update_field
     @field=ProductField.find(params[:id])
     @field.categories=[params[:product_field][:categories]]
-    @field.update_attributes(fields_white_list)
-    @field.save
-    flash[:notice]=[5000,t('admin.toast.field_update')]
-    redirect_to :back
+render json: @field
+    # @field.update_attributes(fields_white_list)
+    # @field.save
+    # flash[:notice]=[5000,t('admin.toast.field_update')]
+    # redirect_to :back
   end
   
   def delete_field
