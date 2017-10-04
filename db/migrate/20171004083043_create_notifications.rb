aef6b67e35b56255dc90ecfcf7b261c4913a1302
mac <mm580486@gmail.com>
2017-10-04 12:03:29 +0330

    update schem

--- db/migrate/20171004083043_create_notifications.rb
+++ db/migrate/20171004083043_create_notifications.rb
@@ -1,12 +1,16 @@
 class CreateNotifications < ActiveRecord::Migration
-  def change
+  def up
     create_table :notifications do |t|
-      t.integer :user_id
-      t.integer :type
-      t.string :body
-      t.boolean :seen
+      t.integer :user_id,null: false
+      t.integer :type,null: false,default: 0
+      t.string :body,null: false
+      t.boolean :seen,default: false
 
       t.timestamps null: false
     end
+  end
+
+  def down
+    drop_table :notifications
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170826080914) do
+ActiveRecord::Schema.define(version: 20171004083043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,15 @@ ActiveRecord::Schema.define(version: 20170826080914) do
     t.string   "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.integer  "user_id",                    null: false
+    t.integer  "type",       default: 0,     null: false
+    t.string   "body",                       null: false
+    t.boolean  "seen",       default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   create_table "pages", force: :cascade do |t|
